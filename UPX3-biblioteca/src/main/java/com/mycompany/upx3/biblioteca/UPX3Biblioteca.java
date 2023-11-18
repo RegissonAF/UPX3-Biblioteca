a7884b76f644e283b6cf744a2ee946dee436b094
Regisson Andre de Lara Filho <ReleafAF@gmail.com>
2023-11-18 20:02:20 -0300

    Creating the first draft for the interface.

---
+++ UPX3-biblioteca/src/main/java/com/mycompany/upx3/biblioteca/UPX3Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.upx3.biblioteca;
+
+/**
+ *
+ * @author releaf
+ */
+public class UPX3Biblioteca {
+
+    public static void main(String[] args) {
+
+    }
+}
